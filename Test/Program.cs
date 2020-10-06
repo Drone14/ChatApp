621b1df69969fbabdcf81469e988bcab0522f957
Jared Whitlock <jdw71498@live.com>
2020-10-06 19:21:38 -0400

    Fix HexStringToByteArray

--- Test/Program.cs
+++ Test/Program.cs
@@ -32,7 +32,7 @@ namespace Test
         {
             byte[] bytes = new byte[hex.Length / 2];
 
-            for(int i = 0; i < bytes.Length; i += 2)
+            for(int i = 0; i < hex.Length; i += 2)
                 bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
 
             return bytes;
